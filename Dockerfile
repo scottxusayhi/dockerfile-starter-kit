72b8975bbb0af72e92f695b0abdbdd5f51c5bd62
Di Xu <xudi@ict.ac.cn>
2017-05-27 20:51:19 +0800

    save env-list.yaml to label as metadata

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,15 @@
 FROM dev.k2data.com.cn:5001/ubuntu:14.04
-ENTRYPOINT []
-CMD []
-
 ARG branch
 ARG commit
 ARG buildtime
 ARG owner
+ARG env_para
 LABEL branch=$branch \
         commit=$commit \
         buildtime=$buildtime \
-        owner=$owner
+        owner=$owner \
+        env_para=$env_para
+# put your commands here
+
+ENTRYPOINT []
+CMD []
--- docker/build.sh
+++ docker/build.sh
@@ -7,8 +7,12 @@ docker build -t ${IMAGE} \
   --build-arg branch=${GIT_BRANCH} \
   --build-arg commit=$(git rev-parse HEAD) \
   --build-arg buildtime="$(date +"%Y-%m-%d %T")"  \
-  --build-arg owner=${IMAGE_OWNER} ${SCRIPT_DIR}/../
+  --build-arg owner=${IMAGE_OWNER} \
+  --build-arg env_para="$(cat $SCRIPT_DIR/env-list.yaml)" \
+  ${SCRIPT_DIR}/../
 
 echo -e "\n$IMAGE"
-docker inspect $IMAGE | grep LABEL
+docker inspect --format "branch={{.Config.Labels.branch}} commit={{.Config.Labels.commit}} buildtime={{.Config.Labels.buildtime}} owner={{.Config.Labels.owner}}" $IMAGE
+echo "env_para="
+docker inspect --format "{{.Config.Labels.env_para}}" $IMAGE
 
